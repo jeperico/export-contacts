315ef7b668b0379dea0eaa564a1d737182647510
jeperico <joaoeperico@gmail.com>
2025-07-15 14:12:24 -0300

    [style] remove wrong void lines

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ with open('input.json', 'r', encoding='utf-8-sig') as file:
 
 with open('contact-list.csv', 'w', newline='', encoding='utf-8-sig') as file:
   writter = csv.writer(file)
-  
   writter.writerow([
     "id",
     "name",
@@ -31,4 +30,3 @@ with open('contact-list.csv', 'w', newline='', encoding='utf-8-sig') as file:
     ]
     writter.writerow(row)
     count += 1
-
